0da599d1d6605b5dd729e9bacf89e8f45aaf5cf4
romulogarofalo <romulogarofalo@gmail.com>
2022-07-14 13:59:43 -0300

    add config to use mox

--- mix.exs
+++ mix.exs
@@ -21,6 +21,10 @@ defmodule Apostila.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:mox, "~> 0.5.2", only: :test},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
+    # optional, but recommended adapter
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
